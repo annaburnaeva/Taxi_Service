3e400126e150c9ff0da0f5f354492eb06f82038b
student <student@localhost>
2019-09-23 15:07:05 +0300

    Added decision

---
+++ src/main/java/TaxiService.java
@@ -0,0 +1,17 @@
+public class TaxiService {
+    int predict(int amountOfPlanting, int costPerKilometer, int costOfOrderForDiscount, int discountPercent, int maxDiscount, int discount, int distance) {
+        int costOfOrder = amountOfPlanting + costPerKilometer * distance;
+
+        if (costOfOrder > costOfOrderForDiscount) {
+            discount = costOfOrder * discountPercent / 100;
+            if (discount >= maxDiscount)
+                discount = maxDiscount;
+            else
+                discount = discount;
+        } else
+            discount = 0;
+
+        int finalyCostOfOrder = costOfOrder - discount;
+        return finalyCostOfOrder;
+    }
+}
