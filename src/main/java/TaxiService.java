7d3f89f351826196652423a58079f5f466e11082
student <student@localhost>
2019-09-25 11:19:02 +0300

    Fixes #3 Removed block "else" with unnecessary assignment

--- src/main/java/TaxiService.java
+++ src/main/java/TaxiService.java
@@ -1,7 +1,6 @@
 public class TaxiService {
     int predict(int amountOfPlanting, int costPerKilometer, int costOfOrderForDiscount, int discountPercent, int maxDiscount, int discount, int distance) {
         int costOfOrder = amountOfPlanting + costPerKilometer * distance;
-
         if (costOfOrder > costOfOrderForDiscount) {
             discount = costOfOrder * discountPercent / 100;
             if (discount >= maxDiscount) {
@@ -10,7 +9,6 @@ public class TaxiService {
         } else {
             discount = 0;
         }
-
         int finalyCostOfOrder = costOfOrder - discount;
         return finalyCostOfOrder;
     }
